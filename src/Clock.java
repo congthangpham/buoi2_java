19d9d36234c5d15cbae36c50d20337ac1ca89b9e
bobaonhan123 <hosybaonhan2004@gmail.com>
2024-02-22 07:23:48 +0700

    clock

---
+++ src/Clock.java
@@ -0,0 +1,31 @@
+import javax.swing.*;
+import java.awt.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
+
+public class Clock extends JPanel {
+    private JLabel timeLabel;
+
+    public Clock() {
+        setBounds(0,0,200,100);
+        setLayout(new FlowLayout());
+
+        timeLabel = new JLabel();
+        add(timeLabel);
+    }
+
+    public void Start() {
+        while (true) {
+            updateTime();
+        }
+    }
+
+    private void updateTime() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+        timeLabel.setText(dateFormat.format(new Date()));
+    }
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,32 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+public class Main extends JFrame {
+    public Main() {
+        setTitle("Simple Clock");
+        setSize(200, 150);
+        setLayout(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+        JButton bt = new JButton("button");
+        bt.setBounds(50,70,100,30);
+        add(bt);
+        Clock clock = new Clock();
+        add(clock);
+        clock.Start();
+        bt.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("clicked");
+            }
+        });
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    public static void main(String[] args) {
+        new Main();
     }
 }
